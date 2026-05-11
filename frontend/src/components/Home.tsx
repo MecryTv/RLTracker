a399baca592acbf35e48087eb2486aab905c56dc
MecryTv <mail.MecryTv@gmail.com>
2026-05-11 17:05:57 +0200

    [ADD] Creating Home Component

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import '../styles/Home.scss';
+
+export default function Home() {
+  return (
+    <div className="home" id="home">
+        <section className="hero">
+            <div className="hero-bg"></div>
+            <div className="hero-grid"></div>
+        </section>
+    </div>
+  );
+}
